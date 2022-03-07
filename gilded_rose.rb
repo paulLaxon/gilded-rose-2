89b731559944d7965d6889618b9a8422694adcf7
Paul Laxon <pdl1963@gmail.com>
2022-03-07 07:50:42 -0500

    refactoring partially done

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # class GildedRose
 class GildedRose
   attr_accessor :items
@@ -8,56 +10,38 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            if item.name == 'Conjured Mana Cake'
-              item.quality = item.quality - 2
-            else
-              item.quality = item.quality - 1
-            end
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+      item.sell_in -= 1
+
+      if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality.positive?
+          item.quality = if item.name == 'Conjured Mana Cake'
+                           item.quality - 2
+                         else
+                           item.quality - 1
+                         end
+        end
+      elsif item.quality < 50
+        item.quality += 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.sell_in <= 11
+            item.quality += 1
+          end
+          if item.sell_in <= 6
+            item.quality += 1
           end
         end
+      end
+      next unless item.sell_in.negative?
+
+      item.quality += 1 if item.name == 'Aged Brie' && item.quality < 50
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality = 0
+      elsif item.quality.positive? && item.name == 'Conjured Mana Cake'
+        item.quality -= 2
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                if item.name == 'Conjured Mana Cake'
-                  item.quality = item.quality - 2
-                else
-                  item.quality = item.quality - 1
-                end
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        item.quality -= 1
       end
     end
   end
